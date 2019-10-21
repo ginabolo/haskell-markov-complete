359b57702aef4c0fefaf6d0235e944c87571e5ae
Andy Ma <solarcactus@live.com>
2019-10-20 21:03:37 -0700

    Added some new display prompts, fixed some errors

--- Main.hs
+++ Main.hs
@@ -5,16 +5,23 @@ import Request
 displayRawWeatherData :: IO()
 displayRawWeatherData = do
     putStrLn "Raw Weather Data: "
-    putStrLn getRawWeatherData
+    data <- getRawWeatherData
+    putStrLn data
 
 displayInfinitePrediction :: IO()
 displayInfinitePrediction = do
+    putStrLn "Welcome to the CPSC 312 Weather Predictor!"
+    weatherSummary <- getWeatherForecastSummary
+    putStrLn "In the last 50 hours, the weather has been: " ++ show (weatherSummary)
     putStrLn "Infinite markov chain prediction with past 50 hours of data: "
     putStrLn (nextPrediction getWeatherForecastSummary)
 
-displayTimedPrediction :: IO()
-displayTimedPrediction = do
-    putStrLn "Enter the number of iterations in the markov chain: "
+displayPrediction :: IO()
+displayPrediction = do
+    putStrLn "Welcome to the CPSC 312 Weather Predictor!"
+    weatherSummary <- getWeatherForecastSummary
+    putStrLn "In the last 50 hours, the weather has been: " ++ show (weatherSummary)
+    putStrLn "Please enter the # of iterations in markov chain predictor: "
     iterations <- getLine
     putStrLn "Weather prediction for the next " ++ (show (* (read iterations) 50)) ++ " hours: "
     putStrLn (nextPrediction getWeatherForecastSummary (read iterations))
@@ -23,8 +30,9 @@ displayAllDaysWithAttribute :: IO()
 displayAllDaysWithAttribute = do
     putStrLn "Enter a weather attribute: "
     attribute <- getLine
+    data <- getRawWeatherData
     putStrLn "All data for <" ++ attribute ++ ">: " 
-    putStrLn (GetAllMatching attribute)  
+    putStrLn (getAllMatching (extractKeyValue data))
 
 nextPrediction data = markov data ++ (nextPrediction (markov data))
 predictWeather data iteration | iteration > 0 = markov data ++ (predictWeather (markov data) (iteration - 1))
