c65359c7c8cc693cff24d957a18a665d896d9141
Archiving <solarcactus@live.com>
2019-10-21 13:44:42 -0700

    new changes

--- Main.hs
+++ Main.hs
@@ -11,13 +11,16 @@ displayRawWeatherData = do
 --displayInfinitePrediction :: IO()
 displayInfinitePrediction = do
     putStrLn "Welcome to the CPSC 312 Weather Predictor!"
-    weatherSummary <- getWeatherForecastSummary
+    weatherSummary <- getWeatherForecastSummaryVerbose False
     putStrLn "In the last 50 hours, the weather has been: "
     putStrLn (show weatherSummary)
     putStrLn "Infinite markov chain prediction with past 50 hours of data: "
-    putStrLn (nextPrediction weatherSummary)
+    next <- nextPrediction weatherSummary
+    putStrLn next
 
-nextPrediction dataW
+nextPrediction dataW = do
+    next <- (markov dataW)
+    return next
 
 inifinitePrediction dataW = do
     next <- markov dataW
