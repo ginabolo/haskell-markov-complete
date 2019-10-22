fc4e908b9116c01c7fb153d81313f4d2c05c6c5a
Andy Ma <solarcactus@live.com>
2019-10-21 20:45:22 -0700

    Can now write raw weather data into file, interfacing changes

--- Main.hs
+++ Main.hs
@@ -4,25 +4,52 @@ import Request
 import Data.List
 import Data.Char
 import Data.Time
+import System.Directory
 
-displayRawWeatherData = do
+
+displayRawWeatherData weatherData = do
     putStrLn "Raw Weather Data: "
-    dataW <- getRawWeatherForecast
-    putStrLn dataW
+    putStrLn weatherData
 
+writeRawWeatherData weatherData = do
+    putStrLn "Enter the filename you want to write to (without extension): "
+    filename <- getLine 
+    path <- getCurrentDirectory
+    putStrLn ("Writing to " ++ path ++ "\\" ++ filename ++ ".txt")
+    writeFile (filename ++ ".txt") weatherData
+    displayRawWeatherData weatherData
+
+getRawWeatherData = do
+    putStrLn "Loading Raw Weather Data....."
+    dataW <- getRawWeatherForecast
+    putStrLn "Would you like to write data to textfile? (y/n): "
+    answer <- getLine 
+    case (toLower (head answer)) of
+        'y' -> writeRawWeatherData dataW
+        'n' -> displayRawWeatherData dataW
+        _ -> error "Invalid Input!"
+    
 main = do
-    weatherSummary <- getWeatherForecastSummaryVerbose False
+    putStrLn "================================================="
     putStrLn "Welcome to CPSC 312 Weather Predictor"
     putStrLn "What would you like to do?"
     putStrLn "0 - Get raw weather data"
     putStrLn "1 - Get weather forecast summary"
     putStrLn "2 - Get weather prediction for next day"
     putStrLn "3 - Get weekly weather prediction"
+    putStrLn "4 - Get infinite prediction [WARNING - Will not stop unless you manually exit]"
+    putStrLn "================================================="
     option <- getLine
-    if (option = '0') then displayRawWeatherData else putStrLn ""
-    if (option = '1') then (show weatherSummary) else putStrLn ""
-    if (option = '2') then displayNextPrediction else putStrLn ""
-    if (option = '3') then displayWeeklyPrediction else putStrLn ""
+    weatherSummary <- getWeatherForecastSummaryVerbose False
+    case (read option) of 
+        0 -> getRawWeatherData
+        1 -> putStrLn (show weatherSummary)
+        2 -> displayNextPrediction
+        3 -> displayWeeklyPrediction
+        4 -> infinitePrediction
+        _ -> error "Error on parsing input!"
+    putStrLn "Done! \n"
+    main
 
     
 displayNextPrediction = do
@@ -38,6 +65,7 @@ displayNextPrediction = do
 
 displayWeeklyPrediction = do
     putStrLn "The weekly weather forecast has been: "
+    weatherSummary <- getWeatherForecastSummaryVerbose False
     putStrLn (show weatherSummary)
 
 nextPrediction dataW = do
@@ -47,5 +75,3 @@ nextPrediction dataW = do
 infinitePrediction dataW = do
     next <- markov dataW
     infinitePrediction (dataW ++ [next])
-
-main = do putStrLn ""
